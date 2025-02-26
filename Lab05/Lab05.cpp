bbef023298a21718839f740183aa7cf766337d34
wndudrla0211 <alex99.young.kim@gmail.com>
2025-02-25 20:38:14 -0500

    1

--- Lab05/Lab05.cpp
+++ Lab05/Lab05.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 char translateChar(char c) {
     vector<char> codeTable = { 'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' };
-
     if (c >= 65 && c <= 90) {  
         return codeTable[c - 65];
     }
@@ -24,12 +23,10 @@ int main() {
     string text;
     cout << "Enter text to encode (up to 128 characters): ";
     getline(cin, text);
-
     string encoded;
     for (char c : text) {
         encoded += translateChar(c);
     }
-
     cout << "Encoded message: " << encoded << endl;
     return 0;
 }
